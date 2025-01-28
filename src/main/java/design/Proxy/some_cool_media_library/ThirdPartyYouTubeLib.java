8a7bb41e33ea2fa9037a4365193c01b67325c18d
XinYThomas <thomasedvans6@gmail.com>
2025-01-29 02:38:46 +0800

    代理模式

---
+++ src/main/java/design/Proxy/some_cool_media_library/ThirdPartyYouTubeLib.java
@@ -0,0 +1,10 @@
+package design.Proxy.some_cool_media_library;
+
+import java.util.HashMap;
+
+
+public interface ThirdPartyYouTubeLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}
